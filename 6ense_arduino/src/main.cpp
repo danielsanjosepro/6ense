8ad6be4c9bb285a34a9fd82ccd78fcc8978754d6
MaxiTrien <51158324+MaxiTrien@users.noreply.github.com>
2021-06-26 17:04:25 +0200

    Add basic imu output in serial monitor.

--- 6ense_arduino/src/main.cpp
+++ 6ense_arduino/src/main.cpp
@@ -2,20 +2,24 @@
 // 6ense includes
 // #include "I2CScanner.hpp"
 #include "Display.h"
+#include "imu.h"
 
 #define SERIAL_BAUDRATE 9600
 
 // auto i2c_scanner = I2CScanner();
 auto display = Display(0x27, 16, 2);
+auto imu = Imu();
 
 void setup()
 {
     Serial.begin(SERIAL_BAUDRATE);  // start serial communication for other Objects
     // i2c_scanner.setup();
     display.setup();
+    imu.setup();
 } 
 
 void loop() {
     // i2c_scanner.loop();
     display.loop();
+    imu.loop();
 }
