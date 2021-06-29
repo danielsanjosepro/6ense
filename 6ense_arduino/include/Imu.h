13ebd017572cdcf2025f42ce55b7bc767c4ff993
MaxiTrien <51158324+MaxiTrien@users.noreply.github.com>
2021-06-29 21:08:06 +0200

    Current Velocity
    Function for computation of velocity in realtime. Get current, last
    timestamp and acceleration from the imu to calculate the velocity for
    a delta t range. Do this for each axis of acceleration.
    
    Tests for loop blockage need to be done to see the time for each
    function call.

--- 6ense_arduino/include/Imu.h
+++ 6ense_arduino/include/Imu.h
@@ -16,10 +16,27 @@ public:
     // Constructor
     Imu();
 
+    // Function to compute velocity for delta t from the accelerometers component's
+    float compute_current_velocity(float& velocity_last, const float& acc, 
+                                   float& time_current, float& time_last){
+        float dv = 0;
+        float velocity = 0;
+        
+        dv = acc * (time_current - time_last);
+        velocity = dv + velocity_last;
+        time_last = time_current;
+
+        return velocity;
+    }
+
+
 private:
-    float x, y, z;
-    int deg_x = 0;
-    int deg_y = 0;
+    float acc_x, acc_y, acc_z; // In units of g 
+    float gyro_x, gyro_y, gyro_z; // In units of degrees per sec
+    float roll, pitch, yaw; // Orientation angles
+    float velocity_x = 0.0; // Inital velocity
+    float velocity_y = 0.0;
+    float velocity_z = 0.0;
 
 };
 
