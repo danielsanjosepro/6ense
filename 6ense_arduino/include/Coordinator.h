ee65ec2188d7ef95070dcf593bede8f102f29824
danielsanjosepro <sanjose.daniel@gmail.com>
2021-06-29 21:31:05 +0200

    inplemented the simple functionality for the loop coordinator

---
+++ 6ense_arduino/include/Coordinator.h
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include "Timer.h"
+
+/**
+ *  Coordinators schedule the loop time for every object in the loop.
+ *  Usage:
+ *  imuCoordinator(100);  // the imu will be called every 100 ms
+ *  ...
+ *  if(imuCoordinator.allowsLoop())
+ *      imu.loop()
+*/
+class Coordinator {
+public:
+    Coordinator(int period_ms);
+
+    void init();
+    bool allowsLoop();
+private:
+    int period;
+    Timer timer;
+};
